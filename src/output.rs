301f64cb256fb017c245beefacae4a882a0e42c7
Subhan <68732277+delamarch3@users.noreply.github.com>
2025-07-18 15:04:48 +0100

    format data

--- src/output.rs
+++ src/output.rs
@@ -12,11 +12,9 @@ pub struct Output {
     text: Vec<u8>,
 }
 
-// TODO: Interpreter takes Out
 // TODO: Debugger - map position to line?
 
 impl std::fmt::Display for Output {
-    // TODO: format data
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         const TAB_SPACES: usize = 4;
 
@@ -30,20 +28,48 @@ impl std::fmt::Display for Output {
             write!(f, "{operand}")
         }
 
+        let next_position = |pc: &Program<'_>| {
+            pc.position() + mem::size_of::<u64>() as u64 + self.data.len() as u64
+        };
+
+        // Write entry
         if let Some(entry) = self.labels.get(&self.entry) {
             writeln!(f, ".entry {}", entry)?;
         } else {
             writeln!(f, ".entry {}", self.entry)?;
         }
+        writeln!(f)?;
 
+        // Write data
+        for (i, chunk) in self.data.as_slice().chunks(16).enumerate() {
+            let pos = i + mem::size_of::<u64>();
+
+            write!(f, "{pos}: ")?;
+            for b in chunk {
+                write!(f, "{:x} ", b)?;
+            }
+
+            write!(f, "| ")?;
+            for b in chunk {
+                if b.is_ascii_graphic() {
+                    write!(f, "{}", *b as char)?
+                } else {
+                    write!(f, ".")?
+                }
+            }
+            writeln!(f)?;
+        }
+        writeln!(f)?;
+
+        // Write text
         let mut pc = Program::new(&self.text);
-        let mut pos = pc.position(); // TODO: position should count entry and data len
+        let mut pos = next_position(&pc);
         while let Ok(op) = pc.next_op() {
-            if let Some(label) = self.labels.get(&(pos + 8)) {
+            if let Some(label) = self.labels.get(&(pos)) {
                 write!(f, "{label}:\n")?;
             }
 
-            write!(f, "{:TAB_SPACES$}{}: ", "", pos + 8)?;
+            write!(f, "{:TAB_SPACES$}{}: ", "", pos)?;
 
             match op {
                 Bytecode::Push => fmt_with_operand::<i32>(f, &mut pc, "push")?,
@@ -87,8 +113,8 @@ impl std::fmt::Display for Output {
                 Bytecode::RetD => write!(f, "ret.d")?,
             }
 
+            pos = next_position(&pc);
             writeln!(f)?;
-            pos = pc.position();
         }
 
         Ok(())
@@ -153,7 +179,13 @@ mod test {
         let src = "
 .entry main
 
+.data record
+    .string \"abc\"
+    .byte 0
+    .word 76
+
 main:
+    push.d record
     push 22
     push 33
     call add
@@ -170,17 +202,21 @@ add:
         let have = output.to_string();
         let want = "\
 .entry main
+
+8: 61 62 63 0 4c 0 0 0 | abc.L...
+
 main:
-    8: push 22
-    13: push 33
-    18: call 37
-    27: store 0
-    36: ret
+    16: push.d 8
+    25: push 22
+    30: push 33
+    35: call 54
+    44: store 0
+    53: ret
 add:
-    37: load 0
-    46: load 1
-    55: add
-    56: ret
+    54: load 0
+    63: load 1
+    72: add
+    73: ret
 ";
 
         assert_eq!(want, have);
